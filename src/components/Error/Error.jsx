30a8fa93bab05c8e6d4ace0f48193838433a3c2c
Timur Marokko <proofblame@yandex.ru>
2021-11-14 01:02:33 +0300

    404

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,47 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import nlo from '../../images/nlo.png'
 
 function Error() {
+  useEffect(() => {
+    document.title = "Error"
+  }, []);
   return (
     <main className="main">
-    <div className="container">
-      <div className="wrapper">
+      <div className="container">
+        <div className="wrapper">
 
 
 
-        <section className="banner">
-          <div className="banner__wrapper banner__wrapper_type_two-columns">
-            <div className="wrapper profile__body">
-              <h1 className="banner__title title">Oops...
-              </h1>
-              <h1 className="banner__title title " style={{color: "rgba(36, 36, 36, 0.58);"}}>Oops...</h1>
-             
+          <section className="banner">
+            <div className="banner__wrapper banner__wrapper_type_two-columns">
+              <div className="wrapper profile__body">
+                <h1 className="banner__title title">Oops...
+                </h1>
+                <h1 className="banner__title title " style={{ color: "rgba(36, 36, 36, 0.58);" }}>Oops...</h1>
+
+                <div className="wrapper">
+                  <div className="image image_type_mobile">
+                    <picture>
+                      <img src={nlo}
+                        alt="Error" />
+                    </picture>
+                  </div>
+                </div>
+
+              </div>
               <div className="wrapper">
-                <div className="image image_type_mobile">
+                <div className="image image_type_desktop">
                   <picture>
                     <img src={nlo}
-                      alt="Error" />
+                      alt="Делегируй монеты со своего устройства и начни зарабатывать уже через 24 часа!" />
                   </picture>
                 </div>
-              </div>
 
-            </div>
-            <div className="wrapper">
-              <div className="image image_type_desktop">
-                <picture>
-                  <img src={nlo}
-                    alt="Делегируй монеты со своего устройства и начни зарабатывать уже через 24 часа!" />
-                </picture>
               </div>
-              
             </div>
-          </div>
-          
-        </section>
 
+          </section>
+
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
   )
 }
 
