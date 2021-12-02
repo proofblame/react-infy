f6c7be3de95f53b3294d3d55ba80fd32dc68bfba
Alexander Vitchinov <vitalex92@yandex.ru>
2021-12-02 18:45:32 +0300

    textarea chat layout

--- src/components/ChatInput/ChatInput.jsx
+++ src/components/ChatInput/ChatInput.jsx
@@ -1,5 +1,19 @@
+import "./ChatInput.scss";
+
 function ChatInput() {
-  return <section className="chatInput">chatInput</section>;
+  return (
+    <section className="chatInput">
+      <form action="" className="chat-form">
+        <textarea
+          name=""
+          id=""
+          className="chat-form__textarea"
+          placeholder="Написать сообщение..."
+        ></textarea>
+        <button type="submit" className="chat-form__submit"></button>
+      </form>
+    </section>
+  );
 }
 
 export default ChatInput;
