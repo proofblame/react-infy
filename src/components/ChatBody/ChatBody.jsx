717541d377360a808c7a29c7e4b89d1bfd20ce6e
Alexander Vitchinov <vitalex92@yandex.ru>
2021-12-04 16:51:20 +0300

    scroll to bottom

--- src/components/ChatBody/ChatBody.jsx
+++ src/components/ChatBody/ChatBody.jsx
@@ -1,10 +1,36 @@
 import "./ChatBody.scss";
 import messages from "../../utils/messages";
 import DialogItem from "../DialogItem/DialogItem";
+import { useEffect, useRef, useState } from "react";
 
 function ChatBody() {
+  const textAreaRef = useRef();
+  const [bodyHeight, setBodyHeight] = useState(0);
+
+  useEffect(() => {
+    countHeight();
+    console.log(bodyHeight);
+    // executeScroll();
+    return () => {
+      setBodyHeight(0);
+    };
+  }, []);
+
+  useEffect(() => {
+    executeScroll(bodyHeight);
+    return () => {
+      executeScroll(0);
+    };
+  }, [bodyHeight]);
+  const executeScroll = (data) => {
+    textAreaRef.current.scrollTop = textAreaRef.current.scrollHeight - data;
+  };
+  const countHeight = () => {
+    setBodyHeight(textAreaRef.current.clientHeight);
+  };
+
   return (
-    <section className="chatBody">
+    <section className="chatBody" ref={textAreaRef}>
       {messages.map((m) => {
         return (
           <DialogItem
--- src/components/ChatInput/ChatInput.jsx
+++ src/components/ChatInput/ChatInput.jsx
@@ -1,29 +1,16 @@
-import { useRef } from "react";
 import "./ChatInput.scss";
 
 function ChatInput() {
-  const textRef = useRef();
-  function SetNewSize() {
-    if (textRef.current.value.length > 5) {
-      textRef.current.cols = 50;
-      textRef.current.rows = 50;
-    } else {
-      textRef.current.cols = 10;
-      textRef.current.rows = 15;
-    }
-  }
-
   return (
     <section className="chatInput">
       <form action="" className="chat-form">
         <textarea
           name=""
           id=""
-          ref={textRef}
-          onChange={SetNewSize}
           className="chat-form__textarea"
           placeholder="Написать сообщение..."
         ></textarea>
+
         <button type="submit" className="chat-form__submit"></button>
       </form>
     </section>
