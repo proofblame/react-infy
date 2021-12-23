12e7c9b4cccfb9436e549d7197f98f0734c0aadb
Timur Marokko <proofblame@gmail.com>
2021-12-23 19:16:26 +0300

    last

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -68,30 +68,7 @@ function Footer({ onSignOut }) {
               </Link>
             </div>
           </div>
-          <nav className="footer__nav">
-            <ul className="menu">
-              <li className="footer__item"><Link to="/about" className="footer__link text text_size_normal">О компании</Link></li>
-              <li className="footer__item"><Link to="/roadmap" className="footer__link text text_size_normal">Дорожная карта</Link></li>
-              <li className="footer__item text text_size_normal"><Link to="/support" className="footer__link text text_size_normal">Поддержка</Link></li>
-            </ul>
-            <ul className="footer__contacts">
-              <li className="footer__item text text_size_normal">По всем вопросам <br /> info@infy-corp.com </li>
-            </ul>
-          </nav>
-          <div className="wrapper">
-            <div className="footer__buttons">
-              <Link to="/profile" className="footer__button link link_active">
-                Профиль
-              </Link>
-              <Link
-                to="/login"
-                className="footer__button link"
-                onClick={onSignOut}
-              >
-                Выход
-              </Link>
-            </div>
-          </div>
+
         </footer>
       ) : (
         <>
