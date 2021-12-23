fb9f414816fa2cda4baba816bfbc90348baa2cb7
Timur Marokko <proofblame@gmail.com>
2021-12-23 19:11:03 +0300

    merged moder

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Footer.css";
-import Logo from "../Logo/Logo";
-
-import React from 'react';
 import {
   Link,
   useLocation
